0094e0a77fb0bcde516e6d38be4332179a1ca780
namingbe <namingbe@users.noreply.github.com>
2025-02-07 15:28:47 +0100

    4.5.0

--- Scanner.scala
+++ Scanner.scala
@@ -1,13 +1,14 @@
 import scala.collection.mutable
+import TokenType.*
 
 class Scanner(val source: String) {
-  val tokens = mutable.ArrayBuffer[Token]()
+  private val tokens = mutable.ArrayBuffer[Token]()
   // first char in the lexeme being scanned
-  var start = 0
+  private var start = 0
   // char currently considered, start ≤ current
-  var current = 0
+  private var current = 0
   // line in the source
-  var line = 1
+  private var line = 1
 
   def scanTokens(): List[Token] = {
     while (current < source.length) {
@@ -18,5 +19,30 @@ class Scanner(val source: String) {
     tokens.toList
   }
 
-  def scanToken(): Unit = ???
+  private def scanToken(): Unit = {
+    val c = advance()
+    c match {
+      case '(' => addToken(LEFT_PAREN)
+      case ')' => addToken(RIGHT_PAREN)
+      case '{' => addToken(LEFT_BRACE)
+      case '}' => addToken(RIGHT_BRACE)
+      case ',' => addToken(COMMA)
+      case '.' => addToken(DOT)
+      case '-' => addToken(MINUS)
+      case '+' => addToken(PLUS)
+      case ';' => addToken(SEMICOLON)
+      case '*' => addToken(STAR)
+    }
+  }
+
+  private def advance(): Char = {
+    val c = source.charAt(current)
+    current += 1
+    c
+  }
+
+  private def addToken(tokenType: TokenType, literal: Any = null): tokens.type = {
+    val text = source.substring(start, current)
+    tokens.addOne(Token(tokenType, text, literal, line))
+  }
 }
