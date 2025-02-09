0466227a9a4ba28d21bb660493a8ee858ea301b4
namingbe <namingbe@users.noreply.github.com>
2025-02-09 14:43:27 +0100

    5.3.3

--- Expr.scala
+++ Expr.scala
@@ -1,8 +1,25 @@
-sealed trait Expr
+sealed trait Expr {
+  def accept[R](visitor: Expr.Visitor[R]): R
+}
 
 object Expr {
-  final case class Binary(left: Expr, operator: Token, right: Expr) extends Expr
-  final case class Grouping(expression: Expr) extends Expr
-  final case class Literal(value: Object) extends Expr
-  final case class Unary(operator: Token, right: Expr) extends Expr
+  final case class Binary(left: Expr, operator: Token, right: Expr) extends Expr {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitBinaryExpr(this)
+  }
+  final case class Grouping(expression: Expr) extends Expr {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitGroupingExpr(this)
+  }
+  final case class Literal(value: Object) extends Expr {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitLiteralExpr(this)
+  }
+  final case class Unary(operator: Token, right: Expr) extends Expr {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitUnaryExpr(this)
+  }
+
+  trait Visitor[R] {
+    def visitBinaryExpr(expr: Expr.Binary): R
+    def visitGroupingExpr(expr: Expr.Grouping): R
+    def visitLiteralExpr(expr: Expr.Literal): R
+    def visitUnaryExpr(expr: Expr.Unary): R
+  }
 }
--- tool/GenerateAst.scala
+++ tool/GenerateAst.scala
@@ -9,7 +9,6 @@ object GenerateAst {
       System.exit(64)
     }
     val outputDir = args(0)
-    // it's a bit comic with Scala, as output is only a bit longer than this
     defineAst(outputDir, "Expr", List(
       "Binary" -> "left: Expr, operator: Token, right: Expr",
       "Grouping" -> "expression: Expr",
@@ -20,13 +19,24 @@ object GenerateAst {
 
   private def defineAst(outputDir: String, baseName: String, types: List[(String, String)]): Unit = {
     val children = types.map { case (className, fields) =>
-      s"  final case class $className($fields) extends $baseName"
+      s"""  final case class $className($fields) extends $baseName {
+         |    override def accept[R](visitor: Visitor[R]): R = visitor.visit${className}Expr(this)
+         |  }""".stripMargin
     }.mkString("\n")
+    val visitor = types.map { case (className, fields) =>
+      s"def visit${className}Expr(expr: Expr.$className): R"
+    }.mkString("\n    ")
     val complete =
-      s"""sealed trait $baseName
+      s"""sealed trait $baseName {
+         |  def accept[R](visitor: Expr.Visitor[R]): R
+         |}
          |
          |object $baseName {
          |$children
+         |
+         |  trait Visitor[R] {
+         |    $visitor
+         |  }
          |}""".stripMargin
     val writer = new PrintWriter(s"$outputDir/$baseName.scala", "UTF-8")
     writer.print(complete)
