a6f660b8db3f466a2cee4c03fa707c8fedca1c9f
namingbe <namingbe@users.noreply.github.com>
2025-02-09 14:57:58 +0100

    5.4

---
+++ AstPrinter.scala
@@ -0,0 +1,18 @@
+class AstPrinter extends Expr.Visitor[String] {
+  def print(expr: Expr): String = expr.accept(this)
+
+  override def visitBinaryExpr(expr: Expr.Binary): String =
+    parenthesize(expr.operator.lexeme, expr.left, expr.right)
+
+  override def visitGroupingExpr(expr: Expr.Grouping): String =
+    parenthesize("group", expr.expression)
+
+  override def visitLiteralExpr(expr: Expr.Literal): String =
+    if expr.value == null then "nil" else expr.value.toString
+
+  override def visitUnaryExpr(expr: Expr.Unary): String =
+    parenthesize(expr.operator.lexeme, expr.right)
+
+  private def parenthesize(name: String, exprs: Expr*): String =
+    s"($name ${exprs.map(_.accept(this)).mkString(" ")})"
+}
